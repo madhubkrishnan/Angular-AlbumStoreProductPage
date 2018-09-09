ca26eb772473243690c6e3e69c768b5aebeae416
madhubkrishnan <madhubkrishnan@gmail.com>
2018-09-09 19:27:33 +0530

    Completed the module

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -9,6 +9,7 @@ import {ProductService} from '../product.service';
 })
 export class ProductTracklistingComponent implements OnInit {
 albumInfo: Album;
+
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,16 +4,21 @@ import 'rxjs/add/operator/map';
 import { Album } from './album';
 import 'rxjs/Observable';
 import { Observable } from 'rxjs/Observable';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
-  constructor( private _http: Http) { }
+  constructor(private _http: Http) { }
 
-   getAlbum(id: number): Observable<Album>{ 
-    return this._http.get(this._albumUrl).map((response)=><Album>response.json());
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
+  }
 }
